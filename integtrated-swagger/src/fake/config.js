14b0561ed9d035c0a143f1165714b45c344a8463
scott <mic1028002@gmail.com>
2018-12-13 14:40:25 +0800

    add more comments

--- integtrated-swagger/src/fake/config.js
+++ integtrated-swagger/src/fake/config.js
@@ -57,6 +57,8 @@ app.use('/api-v2', jsonServerRouter);
 
 // override some api for making it correspond to your client,
 // if you override path is not existing in json-server-database, it will return 404 status code.
+//
+// You can take a look at json-server/src/router.js:39 ~ 41
 jsonServerRouter.render = (req, res) => {
   if(req.url.match(/^\/posts\/{0,1}$/) && req.method.match(/^GET$/)) {
     res.status(200).jsonp({
@@ -65,7 +67,7 @@ jsonServerRouter.render = (req, res) => {
     });
   }
   else {
-    res.jsonp(res.locals.data);
+    res.jsonp(res.locals.data);  // default render of json-server
   }
 };
 
