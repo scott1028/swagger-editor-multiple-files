fe4500c6ce04f0d6f6678de99badb4afd3a2c029
scott <mic1028002@gmail.com>
2018-12-25 11:12:31 +0800

    remove json-server

--- integrated-swagger-mw-es6-webpack-gulp/src/backend/app.es6.js
+++ integrated-swagger-mw-es6-webpack-gulp/src/backend/app.es6.js
@@ -2,7 +2,6 @@
 
 import express from 'express';
 
-import jsonServerRouterFactory from './json-server';
 import defaultRouterFactory from './default';
 import swaggerRouterFactory from './swagger';
 
@@ -13,6 +12,5 @@ const app = express();
 // Ref: https://stackoverflow.com/questions/8833418/express-js-how-to-make-express-static-have-higher-priority-than-the-rest-of-the/8838618#8838618
 swaggerRouterFactory(app);
 defaultRouterFactory(app);
-jsonServerRouterFactory(app);
 
 export default app;
--- integrated-swagger-mw-es6-webpack-gulp/src/backend/json-server/README.md
+++
@@ -1,4 +0,0 @@
-# What's this for?
-
-- Here is a place for laying source code of json-server style mock api.
-- Something like restful style api you can use json-server to provide mock api.
